381b423fe90ce09fe3482aa23c8dfb8b859183e8
Hansen <hansenzheng@126.com>
2021-02-04 21:47:26 +0800

    update lua

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# hammerspoon-auto-switch-input-method
-auto switching input method between Chinese and English using hammerspoon；使用hammerspoon自动切换输入法
+# 说明
+在 ibreathbsb 同学的代码上，增加了应用启动时的检查并自动切换输入法的功能。
+
+## 使用方式
+将[input-switching.lua]() 中的代码复制到 .hammerspoon/init.lua。
+
+如果要自定义应用程序，直接在脚本里app2Ime处填写应用路径和输入法名字即可。
+
+# 鸣谢
+感谢 ibreathbsb 同学：https://gist.github.com/ibreathebsb/65fae9d742c5ebdb409960bceaf934de
---
+++ input-switching.lua
@@ -0,0 +1,73 @@
+local function Chinese()
+    -- 简体拼音
+    hs.keycodes.currentSourceID("com.apple.inputmethod.SCIM.ITABC")
+end
+
+local function English()
+    -- ABC
+    hs.keycodes.currentSourceID("com.apple.keylayout.ABC")
+end
+
+-- app to expected ime config
+-- app和对应的输入法
+local app2Ime = {
+    {'/Applications/Terminal.app', 'English'},
+    {'/Applications/iTerm.app', 'English'},
+    {'/Applications/Visual Studio Code.app', 'English'},
+    {'/Applications/Xcode.app', 'English'},
+    {'/Applications/MacVim.app', 'English'},
+    {'/Applications/PyCharm.app', 'English'}
+    -- 在这里填写应用路径和对应的输入法
+}
+
+function updateFocusAppInputMethod()
+    local ime = 'English'
+    local front = hs.window.frontmostWindow()
+    if front then
+        local focusAppPath = hs.window.frontmostWindow():application():path()
+        for index, app in pairs(app2Ime) do
+            local appPath = app[1]
+            local expectedIme = app[2]
+
+            if focusAppPath == appPath then
+                ime = expectedIme
+                break
+            end
+        end
+
+        if ime == 'English' then
+            English()
+        else
+            Chinese()
+        end
+    else
+        hs.timer.doAfter(1,updateFocusAppInputMethod)
+    end
+end
+
+-- helper hotkey to figure out the app path and name of current focused window
+-- 当选中某窗口按下ctrl+command+.时会显示应用的路径等信息
+hs.hotkey.bind({'ctrl', 'cmd'}, ".", function()
+    hs.alert.show("App path:        "
+    ..hs.window.focusedWindow():application():path()
+    .."\n"
+    .."App name:      "
+    ..hs.window.focusedWindow():application():name()
+    .."\n"
+    .."BundleID:    "
+    ..hs.window.focusedWindow():application():bundleID()
+    .."\n"
+    .."IM source id:  "
+    ..hs.keycodes.currentSourceID())
+end)
+
+-- Handle cursor focus and application's screen manage.
+-- 窗口激活时自动切换输入法
+function applicationWatcher(appName, eventType, appObject)
+    if eventType == hs.application.watcher.activated or eventType == hs.application.watcher.launched then
+        updateFocusAppInputMethod()
+    end
+end
+
+appWatcher = hs.application.watcher.new(applicationWatcher)
+appWatcher:start()
